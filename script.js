662d4a55f620472fcbe55ee14a0f30675df01057
mernaawd <125801027+mernaawd@users.noreply.github.com>
2024-05-26 00:40:23 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+alert("Welcome to my site")
+do{var x=prompt("Enter your name");
+}
+while(isFinite(x))
+var y=prompt("Choose (red,green,blue)");
+document.write("<h1 style=color:"+y+">" +x+ "</h1>")
